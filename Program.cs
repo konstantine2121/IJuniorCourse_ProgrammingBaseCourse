d24204da386f2ff61479fb0ab6ea2da964fa0333
Konstantine <kach91kach91@gmail.com>
2022-08-21 20:08:43 +0300

    Add SurveyTask.

--- Program.cs
+++ Program.cs
@@ -6,11 +6,14 @@ namespace IJuniorCourse_ProgrammingBaseCourse
     {
         static void Main(string[] args)
         {
-            var lesson1 =  new Variables();
-            lesson1.Run();
+            //var lesson1 =  new Variables();
+            //lesson1.Run();
 
-            var lesson2 = new IntegerDivisionTask();
-            lesson2.Run();
+            //var lesson2 = new IntegerDivisionTask();
+            //lesson2.Run();
+
+            var lesson3 = new SurveyTask();
+            lesson3.Run();
         }
     }
 }
---
+++ ProgrammingBase/SurveyTask.cs
@@ -0,0 +1,56 @@
+﻿using IJuniorCourse_ProgrammingBaseCourse.CommonInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJuniorCourse_ProgrammingBaseCourse.ProgrammingBase
+{
+    /// <summary>
+    ///Вы задаете вопросы пользователю, по типу "как вас зовут", "какой ваш знак зодиака" и тд, после чего, по данным, которые он ввел, формируете небольшой текст о пользователе.<br/>
+    ///<br/>
+    ///"Вас зовут Алексей, вам 21 год, вы водолей и работаете на заводе." 
+    /// </summary>
+    class SurveyTask : IRunnable
+    {
+        public void Run()
+        {
+            const string questionName = "Как вас зовут?";
+            const string questionOld = "Сколько вам лет?";
+            const string questionZodaik = "Какой ваш знак зодиака?";
+            const string questionWork = "Где вы работаете?";
+
+            string name = string.Empty;
+            string old = string.Empty;
+            string work = string.Empty;
+            string zodiak = string.Empty;
+
+            name = AskAQuestion(questionName);
+            old = AskAQuestion(questionOld);
+            work= AskAQuestion(questionWork);
+            zodiak = AskAQuestion(questionZodaik);
+
+            string formattedInfo = $"Вас зовут {name}, вам {old} год, вы {zodiak} и работаете на {work}.";
+
+            Console.WriteLine(formattedInfo);
+        }
+
+        private string AskAQuestion(string question)
+        {
+            string answer = string.Empty;
+            while (string.IsNullOrEmpty(answer))
+            {
+                Console.WriteLine(question);
+                answer = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(answer))
+                {
+                    Console.WriteLine("Ответ слишком короткий. Попробуйте еще раз.");
+                }
+            }
+
+            return answer;
+        }
+    }
+}
